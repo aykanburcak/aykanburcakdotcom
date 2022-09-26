6bc6742091ba2b6aa0f6213b88e4ef62d0bb7e4a
aykan <aykanburcak@gmail.com>
2022-09-26 13:33:18 +0300

    Fixing build errors

--- components/blocks/rich-text/rich-text.js
+++ components/blocks/rich-text/rich-text.js
@@ -2,16 +2,17 @@ import styles from './rich-text.module.scss'
 import Container from "@/components/container/container";
 import ReactMarkdown from "react-markdown";
 
-export const RichText = ({title, content}) => {
+export const RichText = ({title, text_content}) => {
   return (
     <div className={styles.richText}>
       <Container width="medium">
         {title && (
           <h2 className="text-center">{title}</h2>
         )}
-        {content && (
+        {text_content && (
+          /* eslint-disable */
           <div className={styles.richText__content}>
-            <ReactMarkdown children={content}/>
+            <ReactMarkdown children={text_content}/>
           </div>
         )}
       </Container>
--- pages/api/preview.js
+++ pages/api/preview.js
@@ -1,21 +1,25 @@
-import { getPreviewPostBySlug } from '@/lib/api'
+import { getPageData } from "utils/api"
+import { parseCookies } from "utils/parse-cookies"
 
-export default async function preview(req, res) {
+const preview = async (req, res) => {
   // Check the secret and next parameters
   // This secret should only be known to this API route and the CMS
-  if (
-    req.query.secret !== process.env.STRAPI_PREVIEW_SECRET ||
-    !req.query.slug
-  ) {
-    return res.status(401).json({ message: 'Invalid token' })
+  if (req.query.secret !== (process.env.PREVIEW_SECRET || "secret-token")) {
+    return res.status(401).json({ message: "Invalid token" })
   }
 
+  const cookies = parseCookies(req)
+  const slugArray = req.query.slug.split("/")
   // Fetch the headless CMS to check if the provided `slug` exists
-  const post = await getPreviewPostBySlug(req.query.slug)
+  const pageData = await getPageData({
+    locale,
+    slug: slugArray.join("/"),
+    preview: true,
+  })
 
   // If the slug doesn't exist prevent preview mode from being enabled
-  if (!post) {
-    return res.status(401).json({ message: 'Invalid slug' })
+  if (!pageData) {
+    return res.status(401).json({ message: "Invalid slug" })
   }
 
   // Enable Preview Mode by setting the cookies
@@ -23,6 +27,17 @@ export default async function preview(req, res) {
 
   // Redirect to the path from the fetched post
   // We don't redirect to req.query.slug as that might lead to open redirect vulnerabilities
-  res.writeHead(307, { Location: `/posts/${post.slug}` })
+  // Prefix with locale so previews are available in all languages
+  res.writeHead(307, {
+    Location: `/${pageData.locale}/${pageData.slug}`,
+  })
   res.end()
 }
+
+export default preview
+
+// You can view Preview pages with URLs like this:
+// http://localhost:3000/api/preview?secret=<preview-secret>&slug=<slug>
+// where <preview-secret> is the secret token defined in your .env config
+// and where <slug> is the slug you entered in Strapi for your page
+// The slug must match the current locale
