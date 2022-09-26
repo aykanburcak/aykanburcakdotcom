04c92501a6908cc507a3b6cf23519fa9c57f5108
aykan <aykanburcak@gmail.com>
2022-09-26 17:22:36 +0300

    Fix page routing

--- utils/get-data.js
+++ utils/get-data.js
@@ -1,6 +1,6 @@
 export function getData(slug, locale) {
   const slugToReturn = `/${slug}?lang=${locale}`;
-  const apiUrl = `pages?slug=${slug}&_locale=${locale}`;
+  const apiUrl = `pages?filters[slug][$eq]${slug.length > 0 ? `=${slug}` : '%20'}&_locale=${locale}`;
 
   return {
     url: apiUrl,
