ba6fb34cb849b926e7627393bd9d3f0c8cda8a87
aykan <aykanburcak@gmail.com>
2022-10-30 15:03:01 +0300

    Adding language switcher component

--- components/language-switcher/language-switcher.js
+++ components/language-switcher/language-switcher.js
@@ -1,9 +1,22 @@
 import Link from 'next/link'
+import {useLocale} from "../../hooks";
 
 export default function LanguageSwitcher() {
+  const currentLocale = useLocale();
+
   return (
-    <Link href="/" locale="tr">
-      <a>TR</a>
-    </Link>
+    <>
+      {currentLocale === 'en' && (
+        <Link href="/" locale="tr">
+          <a>TR</a>
+        </Link>
+      )}
+
+      {currentLocale === 'tr' && (
+        <Link href="/en" locale="en">
+          <a>EN</a>
+        </Link>
+      )}
+    </>
   )
 }
