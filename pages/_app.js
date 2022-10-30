78a0451f479d61228ce7a99c1b696f82c2d1a75c
aykan <aykanburcak@gmail.com>
2022-10-30 14:49:59 +0300

    Initializing 3rd party Tidio scripy

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,7 @@ import {
   QueryClient,
   QueryClientProvider,
 } from "@tanstack/react-query"
+import Script from "next/script";
 
 const queryClient = new QueryClient()
 
@@ -12,6 +13,10 @@ function App({Component, pageProps}) {
   return (
     <ThemeProvider>
       <QueryClientProvider client={queryClient}>
+        <Script
+          src="//code.tidio.co/nbmrfagrtxy5y7h8tg3ankimrktgsehh.js"
+          strategy="afterInteractive"
+        />
         <Component {...pageProps} />
       </QueryClientProvider>
     </ThemeProvider>
--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,4 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-import Script from "next/script";
 
 export default function Document() {
   return (
@@ -9,10 +8,6 @@ export default function Document() {
         <Main />
         <NextScript />
       </body>
-      <Script
-        src="//code.tidio.co/nbmrfagrtxy5y7h8tg3ankimrktgsehh.js"
-        strategy="lazyOnload"
-      />
     </Html>
   )
 }
